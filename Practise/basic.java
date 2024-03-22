b22c4a31a14054f51569ca73eeb4ff2c4799fc10
NewtonRaja <knewtonraja@gmail.com>
2024-03-22 23:30:26 +0530

    Changes

---
+++ Practise/basic.java
@@ -0,0 +1,14 @@
+//Sum of two numbers
+import java.util.*;
+public class basic
+{
+    public static void main(String[] a)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int num1 = sc.nextInt();
+        int num2=sc.nextInt();
+        int result = num1+num2;
+        System.out.println("Total = " +result);    
+}
+}
