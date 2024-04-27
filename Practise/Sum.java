09f97964b84dbb1720234b630ac2a072bcd9d947
NewtonRaja <knewtonraja@gmail.com>
2024-04-27 22:31:45 +0530

    change

--- Practise/Sum.java
+++ Practise/Sum.java
@@ -2,7 +2,7 @@ class hello{
     public static void main(String a[]){
         int num=2;
         int num1=4;
-        int result = num+num1;
+        int resut = num+num1;
         System.out.print(result);
     }
 }
--- Practise/array.java
+++ Practise/array.java
@@ -1,12 +1,24 @@
-import java.util.*;
-public class array {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String[] list = new String[5];
-        for (int i=0;i<5;i++){
-            list[i]=sc.next();
-        }
+// import java.util.*;
+// public class array {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String[] list = new String[5];
+//         for (int i=0;i<5;i++){
+//             list[i]=sc.next();
+//         }
         
-        sc.close();
-    }
+//         sc.close();
+//     }
+// }
+
+//import java.util.*;
+public static array{
+    public static void main(args[]){
+    int num[][]= new int[4][4];
+    for (int i=0;i<4;i++){
+        for (int j=0;j<4;j++){
+            System.out.println(num[i][j]);
+        }
+    } 
+}
 }
