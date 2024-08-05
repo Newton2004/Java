b65f74689b50de0a83691befe898009f152f6a14
NewtonRaja <knewtonraja@gmail.com>
2024-08-05 15:46:00 +0530

    change

--- Practise/array.java
+++ Practise/array.java
@@ -15,10 +15,11 @@
 public static array{
     public static void main(args[]){
     int num[][]= new int[4][4];
+    int math.random();
     for (int i=0;i<4;i++){
         for (int j=0;j<4;j++){
             System.out.println(num[i][j]);
-        }
+        }  
     } 
 }
 }
--- Practise/basic.java
+++ Practise/basic.java
@@ -6,7 +6,7 @@ public class basic
     {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter two numbers");
-        int num1 = sc.nextInt();
+        int num1=sc.nextInt();
         int num2=sc.nextInt();
         int result = num1+num2;
         System.out.println("Total = " +result);    
