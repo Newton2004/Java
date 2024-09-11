e81795764b1513aa2b841a6da055d24a098080fa
NewtonRaja <knewtonraja@gmail.com>
2024-09-11 21:39:01 +0530

    changes

---
+++ Practise/prime.java
@@ -0,0 +1,11 @@
+public class prime {
+    public static void main(String[] args) {
+        int n=1233,i=2;
+        boolean isprime=true;
+        while(i<n){
+            for(n/2==0){
+            isprime=false;}
+
+        }i++;
+    System.out.println(isprime);
+}
